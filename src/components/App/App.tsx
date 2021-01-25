ea6d2b6553e8840173dbd815eb9e13fc2bec63b8
ilovric00 <ilovric@profico.hr>
2021-01-25 21:11:46 +0100

    Folder structure, using Airbnb's style guide

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from  './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1>Crypto App</h1>
+    </div>
+  );
+}
+
+export default App;
